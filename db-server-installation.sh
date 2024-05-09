439a8a19fbadfb842b0e6206e9f7cd62bcb30cbb
sid2417 <siddhartha.java9398@gmail.com>
2024-05-09 14:36:30 +0530

    db Daily practice

--- db-server-installation.sh
+++ db-server-installation.sh
@@ -1,50 +1,53 @@
-#!/bin/bash/
-
-#sudo access #colors # logfile # timestamp
-#DB server installation
+#!/bin/bash
 
 USERID=$(id -u)
-TIME_STAMP=$(date +%F-%H-%M-%S)
-SRCIPT_NAME=$(echo "$0" | cut -d "." -f1)
-LOG_FILE=/tmp/$SRCIPT_NAME/$TIME_STAMP.log
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "Please enter DB password:"
+read -s mysql_root_password
 
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R Please Provide Super User Access $N"
-    exit 1
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-    echo -e "$G You Already Have sudo Access $N"
+    echo "You are super user."
 fi
 
-VALIDATE ()
-{
-    if [ $1 -ne 0 ]
-then 
-    echo -e "$2 ... $R FAILED $N"
-    exit 1
- else
-    echo -e "$2 ... $G SUCCESS $N"
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL Server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
+
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "Setting up root password"
+
+#Below code will be useful for idempotent nature
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
-
-}
-
-dnf install mysql-server -y &>>LOG_FILE
-VALIDATE $? "MySql Server Installation"
-
-systemctl enable mysqld &>>LOG_FILE
-VALIDATE $? "Enabling MySql server"
-
-systemctl start mysqld &>>LOG_FILE
-VALIDATE $? "Starting MySql server"
-
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOG_FILE
-VALIDATE $? "Password Setup for MySql server"
-
-
-echo -e "$G DB MySql Server Installation was going smoothly..Thankyou $N"
-
