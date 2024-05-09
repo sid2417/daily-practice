ceba5f389e72f4f3413b6f680308fc1a3ca9fb1c
sid2417 <siddhartha.java9398@gmail.com>
2024-05-09 14:40:43 +0530

    db Daily practice

--- comman.sh
+++ comman.sh
@@ -5,9 +5,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-FILENAME=$(echo "$0" | cut -d "." -f1)
+
+FILENAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/tmp/$FILENAME/$TIMESTAMP.log
+
 
 VALIDATE()
 {
