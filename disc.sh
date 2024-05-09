45b84654fc5dd6cc6dec8e6dfe2dbae9ea0e15db
sid2417 <siddhartha.java9398@gmail.com>
2024-05-10 01:35:23 +0530

    Deleting logs

---
+++ disc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash/
+#Threshold limit 15%
+#disk usage column
+#disc usage folder name
+#disk usage column -ge 15 ---> we need to remove data
+
+THRESHOLD_LIMIT=15
+DISC_USAGE_LIST=$(df -hT | grep xfs )
+
+while IFS read -r line
+do 
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $THRESHOLD_LIMIT]
+    then 
+        echo your $DISC_USAGE_LIST is more than $THRESHOLD_LIMIT, from folder Name :  $FOLDER
+    fi
+
+done <<< $DISC_USAGE_LIST
+
+
+
+
+#(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
